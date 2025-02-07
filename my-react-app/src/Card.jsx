869ea4a18e9aa7fa63a507948f952d9a7319cdc5
arunkdorai <arunkumar.k0786@gmail.com>
2025-02-07 21:58:13 +0530

    Card component added

---
+++ my-react-app/src/Card.jsx
@@ -0,0 +1,12 @@
+
+function Card() {
+    return(
+        <div className = "card">
+            <img className="card-image" src="https://images.pexels.com/photos/1759530/pexels-photo-1759530.jpeg?auto=compress&cs=tinysrgb&w=600" alt="profile picture"></img>
+            <h2 className="card-title">Arun</h2>
+            <p className="card-text">Full Stack Developer, MERN Stack</p>
+        </div>
+    );
+}
+
+export default Card
