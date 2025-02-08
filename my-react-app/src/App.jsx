2cafe2333ac11476fbfa0901f6744820031ca274
arunkdorai <arunkumar.k0786@gmail.com>
2025-02-08 11:21:58 +0530

    Props in components

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,8 +1,16 @@
-import Button from './Button.jsx'
+import Student from './Student.jsx'
 
 function App() {
 
-  return(<Button/>)
+  return(
+    <>
+    <Student name = "Spongebob" age={30} isStudent={true} />
+    <Student name = "Patrick" age={42} isStudent={false} />
+    <Student name = "Steve" age={50} isStudent={false} />
+    <Student name = "Trevor" age={27} isStudent={true} />
+    <Student name ="Larry" age="30" />
+    </>
+  )
 }
 
 export default App;
