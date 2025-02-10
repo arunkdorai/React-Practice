068c3ebaaca72cc5ff68926e86ed002e94656b52
arunkdorai <arunkumar.k0786@gmail.com>
2025-02-11 01:39:16 +0530

    Counter component

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,16 +1,8 @@
-import Student from './Student.jsx'
+import Counter from './Counter.jsx'
 
 function App() {
 
-  return(
-    <>
-    <Student name = "Spongebob" age={30} isStudent={true} />
-    <Student name = "Patrick" age={42} isStudent={false} />
-    <Student name = "Steve" age={50} isStudent={false} />
-    <Student name = "Trevor" age={27} isStudent={true} />
-    <Student name ="Larry" age="30" />
-    </>
-  )
+  return(<Counter />)
 }
 
 export default App;
